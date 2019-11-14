7852d8dacb5fe40cc7334dcf16a55362812a0428
Ruchika Joshi <ruchi.joshi2696@gmail.com>
2019-11-13 16:07:49 -0800

    count all palindromes in a given string leetcode medium level

---
+++ Count_Palindromic_Substrings.py
@@ -0,0 +1,58 @@
+# Palindromic Substrings
+
+# Given a string, your task is to count how many palindromic substrings in this string.
+
+# The substrings with different start indexes or end indexes are counted as different substrings even they consist of same characters.
+
+# Example 1:
+
+# Input: "abc"
+# Output: 3
+# Explanation: Three palindromic strings: "a", "b", "c".
+ 
+
+# Example 2:
+
+# Input: "aaa"
+# Output: 6
+# Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+ 
+
+# Note:
+
+# The input string length won't exceed 1000.
+#  
+
+class Solution(object):
+    def countSubstrings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        N = len(s)
+        count = 0
+        #odd length:
+        for i in range(N):
+            for j in range(N):
+                if i-j<0 or i+j>=N:
+                    break
+                
+                if s[i-j] == s[i+j]:
+                     count+=1
+                else:
+                    break
+        
+        #even length
+        for i in range(N):
+            for j in range(N):
+                if i-j<0 or i+1+j>=N:
+                    break
+                if s[i] == s[i+1]:
+                    
+                    if s[i-j] == s[i+1+j]:
+                        count+=1
+                    else:
+                        break
+        
+        return count
+        
