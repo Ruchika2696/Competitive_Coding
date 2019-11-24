a084afcf1bc8e699de68cfbcf30709b997077e4f
Ruchika Joshi <ruchi.joshi2696@gmail.com>
2019-11-23 16:59:24 -0800

    Maximum width of Binary Tree leetcode medium

---
+++ MaximumWidthBinaryTrees.py
@@ -0,0 +1,85 @@
+# Given a binary tree, write a function to get the maximum width of the given tree. The width of a tree is the maximum width among all levels. The binary tree has the same structure as a full binary tree, but some nodes are null.
+
+# The width of one level is defined as the length between the end-nodes (the leftmost and right most non-null nodes in the level, where the null nodes between the end-nodes are also counted into the length calculation.
+
+# Example 1:
+
+# Input: 
+
+#            1
+#          /   \
+#         3     2
+#        / \     \  
+#       5   3     9 
+
+# Output: 4
+# Explanation: The maximum width existing in the third level with the length 4 (5,3,null,9).
+# Example 2:
+
+# Input: 
+
+#           1
+#          /  
+#         3    
+#        / \       
+#       5   3     
+
+# Output: 2
+# Explanation: The maximum width existing in the third level with the length 2 (5,3).
+# Example 3:
+
+# Input: 
+
+#           1
+#          / \
+#         3   2 
+#        /        
+#       5      
+
+# Output: 2
+# Explanation: The maximum width existing in the second level with the length 2 (3,2).
+# Example 4:
+
+# Input: 
+
+#           1
+#          / \
+#         3   2
+#        /     \  
+#       5       9 
+#      /         \
+#     6           7
+# Output: 8
+# Explanation:The maximum width existing in the fourth level with the length 8 (6,null,null,null,null,null,null,7).
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def widthOfBinaryTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        #store node, depth, position
+        q = [(root,0,0)]
+        ans = 0
+        currDepth = 0
+        left = 0
+        for node,depth, position in q:
+            if node:
+                #left
+                q.append((node.left,depth+1,position*2))
+                #right
+                q.append((node.right, depth+1, 1+position*2))
+                if depth!= currDepth:
+                    currDepth = depth
+                    left = position
+                ans = max(position-left+1, ans)
+        return ans  
+        
+        
